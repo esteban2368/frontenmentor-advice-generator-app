c87380d5111cfcb3c3ae5fa7884349a87781c6ee
esteban2368 <esteban.d2168@gmail.com>
2022-04-25 23:48:07 -0500

    to integrate API and to add loader styles

--- dist/index.975ef6c8.js
+++ dist/index.975ef6c8.js
@@ -515,6 +515,39 @@ function hmrAcceptRun(bundle, id) {
 
 },{}],"8lqZg":[function(require,module,exports) {
 var _mainScss = require("./scss/main.scss");
+let response, data;
+const idTxt = document.querySelector("#idTxt");
+const adviceTxt = document.querySelector("#adviceTxt");
+//Events
+window.addEventListener("load", (event)=>{
+    drawLoader();
+    const buttonNewAdvice = document.querySelector(".button");
+    buttonNewAdvice.addEventListener("click", (event)=>{
+        drawLoader();
+        loadAdvice();
+    });
+    loadAdvice();
+});
+//Function async
+const loadAdvice = async ()=>{
+    try {
+        response = await fetch('https://api.adviceslip.com/advice');
+        if (response.status === 200) {
+            data = await response.json();
+            drawAdvice(data.slip.id, data.slip.advice);
+        }
+    } catch (e) {
+        console.log(e);
+    }
+};
+//Function draw advice
+const drawAdvice = (id, advice)=>{
+    idTxt.textContent = id;
+    adviceTxt.innerHTML = `<q>${advice}</q>`;
+};
+const drawLoader = ()=>{
+    adviceTxt.innerHTML = '<div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>';
+};
 
 },{"./scss/main.scss":"4Pg3x"}],"4Pg3x":[function() {},{}]},["7nZVA","8lqZg"], "8lqZg", "parcelRequired69d")
 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
-import "./scss/main.scss"
+import "./scss/main.scss";
+
+let response,
+    data;
+const idTxt = document.querySelector("#idTxt");
+const adviceTxt = document.querySelector("#adviceTxt");
+//Events
+window.addEventListener("load", (event) =>{
+    drawLoader();
+    const buttonNewAdvice = document.querySelector(".button");
+    buttonNewAdvice.addEventListener("click", (event) =>{
+        drawLoader();
+        loadAdvice();
+    })
+
+    loadAdvice();
+})
+
+//Function async
+
+const loadAdvice = async() =>{
+    try{
+        response = await fetch('https://api.adviceslip.com/advice');
+        if(response.status === 200){
+            data = await response.json();
+            drawAdvice(data.slip.id, data.slip.advice);
+        }
+    } catch(e){
+        console.log(e);
+    }
+}
+
+//Function draw advice
+const drawAdvice = (id,advice) =>{
+    idTxt.textContent = id;
+    adviceTxt.innerHTML = `<q>${advice}</q>`;
+}
+
+const drawLoader = () =>{
+
+    adviceTxt.innerHTML = '<div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>'
+}
+
+
